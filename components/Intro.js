1f4c0a7e799f08f96044ccf60ba9f46684336ef2
Keeratipong <keeratipong.b@gmail.com>
2024-11-29 23:20:02 +0700

    fix download button

--- components/Intro.js
+++ components/Intro.js
@@ -78,24 +78,24 @@ function Intro() {
         }}>
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white text-xs sm:text-sm xs:px-7 xs:py-3 px-4 py-1 flex items-center gap-2 rounded-full border border-slate-600 outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("#contact");
             setTimeOfLastClick(Date.now());
           }}>
           Contact me
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+          <BsArrowRight className="hidden xs:block opacity-70 group-hover:translate-x-1 transition" />
           <Mail
             color={"#9ca3af"}
-            className="opacity-70 group-hover:-translate-x-1 transition"
+            className="hidden xs:block opacity-70 group-hover:-translate-x-1 transition"
           />
         </Link>
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white text-xs sm:text-sm xs:px-7 xs:py-3 px-4 py-1 flex items-center gap-2 rounded-full border border-slate-600 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
           download>
           Download CV
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          <HiDownload className="hidden xs:block opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
         <a
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "480px", // Define the custom breakpoint
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
