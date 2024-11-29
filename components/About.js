6187127081c051f08d259bc9d23ae7bae377c5ec
Keeratipong <keeratipong.b@gmail.com>
2024-11-29 23:38:08 +0700

    fix margin top of About me section

--- components/About.js
+++ components/About.js
@@ -11,7 +11,7 @@ export default function About() {
 
   return (
     <motion.section
-      className="max-w-[55rem] text-center mt-32 leading-8 mb-20 sm:mb-28 scroll-mt-28"
+      className="max-w-[55rem] text-center mt-14 leading-8 mb-20 sm:mb-28 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
--- components/Intro.js
+++ components/Intro.js
@@ -21,7 +21,7 @@ function Intro() {
     <section
       ref={ref}
       id="home"
-      className="mb-28 max-w-[75rem] text-center sm:mb-0 scroll-mt-[100rem]">
+      className="mb-8 max-w-[75rem] text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative ">
           <motion.div
