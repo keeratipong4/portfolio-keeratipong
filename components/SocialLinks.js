14bb3597fcd1cbffc04f08c29301883f9e4f98b2
Keeratipong <keeratipong.b@gmail.com>
2024-12-06 01:06:55 +0700

    add repo, liveDemo to data.js

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -15,7 +15,7 @@ export default function SocialLinks() {
     {
       name: "github",
       icon: GithubLogo,
-      color: "",
+      color: "bg-white/40",
       url: "https://github.com/keeratipong4",
     },
 
@@ -23,27 +23,29 @@ export default function SocialLinks() {
       name: "Instagram",
       icon: InstagramLogo,
       // icon: FaInstagram,
-      color: "text-[#764ABC]",
+      color: "dark:bg-white/10",
       url: "https://www.instagram.com/nai_chee/",
     },
     {
       name: "Facebook",
       icon: FacebookLogo,
       // icon: FaFacebookSquare,
-      color: "text-[#0055FF]",
+      color: "dark:bg-white/10",
+      // color: "text-[#0055FF]",
       url: "https://www.facebook.com/cheeblackblue",
     },
     {
       name: "YouTube",
       icon: YoutubeLogo,
       // icon: FaYoutube,
-      color: "text-[#FF4154]",
+      color: "dark:bg-white/10",
+      // color: "text-[#FF4154]",
       url: "https://youtube.com/keeratipong",
     },
     {
       name: "LinkedIn",
       icon: LinkedInLogo,
-      color: "",
+      color: "dark:bg-white/10",
       url: "https://www.linkedin.com/in/keeratipong-boonnapongkasem-99259527b/",
     },
   ];
@@ -57,10 +59,10 @@ export default function SocialLinks() {
             href={social.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="group rounded-full bg-white p-2 shadow-lg transition-shadow duration-300 ease-in-out hover:scale-[1.2] hover:shadow-lg focus:scale-[1.2] active:scale-105 dark:bg-white/10"
+            className={`group rounded-full bg-white p-2 shadow-lg transition-shadow duration-300 ease-in-out hover:scale-[1.2] hover:shadow-lg focus:scale-[1.2] active:scale-105 ${social.color}`}
           >
             <social.icon
-              className={`h-8 w-8 cursor-pointer duration-300 ease-in-out ${social.color} bg-white`}
+              className={`h-8 w-8 cursor-pointer bg-white duration-300 ease-in-out`}
             />
             <span className="absolute -bottom-8 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md bg-gray-500 px-1 py-1 text-xs text-white opacity-0 transition-opacity group-hover:opacity-100 dark:bg-gray-700">
               {social.name}
