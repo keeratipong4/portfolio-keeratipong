69f6e6ee6a603643cf64b3a36bfe1eaf869fc923
Keeratipong <keeratipong.b@gmail.com>
2024-12-09 01:25:26 +0700

    modify education section

--- components/Education.js
+++ components/Education.js
@@ -3,13 +3,22 @@ import SectionHeading from "./SectionHeading";
 import { useSectionInView } from "@/hooks/useInView";
 import { educationData } from "@/lib/data";
 import EducationItem from "./EducationItem";
+import { Fade } from "react-awesome-reveal";
 
 export default function Education() {
   const { ref } = useSectionInView("#education", 0.1);
 
   return (
     <section ref={ref} id="education" className="mb-15 scroll-mt-28">
-      <SectionHeading>Education & Certifications</SectionHeading>
+      <Fade
+        direction="up"
+        delay={400}
+        cascade
+        damping={1e-1}
+        triggerOnce={true}
+      >
+        <SectionHeading>Education & Certifications</SectionHeading>
+      </Fade>
       <div className="flex flex-col gap-8">
         {educationData.map((education, index) => (
           <EducationItem {...education} key={index} />
--- components/EducationItem.js
+++ components/EducationItem.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Fade } from "react-awesome-reveal";
 
 export default function EducationItem({
   institution,
@@ -10,22 +11,40 @@ export default function EducationItem({
 }) {
   return (
     <div className="grid-row-1 grid w-full grid-cols-[80px_1fr] gap-4">
-      <div className="w-auto">
-        <Image src={logo} width={700} height={700} className="" />
-      </div>
-      <div>
-        <h1>{institution}</h1>
-        <h2>{title}</h2>
-        <p>Graduated: {year}</p>
-        {credential && (
-          <Link
-            href={credential}
-            className="flex w-fit items-center gap-1 text-nowrap rounded-full border border-gray-300 bg-gray-100 px-2 py-2 text-sm outline-none transition hover:scale-110 hover:bg-gray-800 hover:text-white focus:scale-110 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-100 dark:hover:text-gray-800 sm:px-3"
-          >
-            Show credential
-          </Link>
-        )}
-      </div>
+      <Fade
+        direction="left"
+        delay={600}
+        cascade
+        damping={1e-1}
+        triggerOnce={true}
+      >
+        <div className="w-auto">
+          <Image src={logo} width={700} height={700} className="" />
+        </div>
+      </Fade>
+      <Fade
+        direction="right"
+        delay={600}
+        cascade
+        damping={1e-1}
+        triggerOnce={true}
+      >
+        <div>
+          <h1 className="font-semibold text-amber-400">{institution}</h1>
+          <h2>{title}</h2>
+          <p className="text-sm text-gray-400 dark:text-gray-500">
+            Graduated: {year}
+          </p>
+          {credential && (
+            <Link
+              href={credential}
+              className="flex w-fit items-center gap-1 text-nowrap rounded-full border border-gray-300 bg-gray-100 px-2 py-2 text-sm outline-none transition hover:scale-110 hover:bg-gray-800 hover:text-white focus:scale-110 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-100 dark:hover:text-gray-800 sm:px-3"
+            >
+              Show credential
+            </Link>
+          )}
+        </div>
+      </Fade>
     </div>
   );
 }
